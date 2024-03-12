90e8471cd41b8cba9059acefaf2ee52eeccf6e0e
Vineet Raj <112626546+MrVineetRaj@users.noreply.github.com>
2024-03-12 16:12:10 +0530

    Update HeroElement.jsx

--- src/components/HeroElement.jsx
+++ src/components/HeroElement.jsx
@@ -12,7 +12,7 @@ const HeroElement = ()=>{
           <p> passionate web developer with expertise in the MERN stack. From crafting interactive user interfaces with React to building robust APIs using Node.js and MongoDB, I thrive on turning ideas into functional, elegant solutions. When I’m not coding, you’ll find me exploring new technologies or sipping coffee at my favorite local café. Let’s create something amazing together!</p>
         </div>
         <div className={`${Style.right}`}>
-          <img src="./public/My photo.png" alt="Loading" />
+          <img src="/My photo.png" alt="Loading" />
         </div>
       </div>
     </>
